a392eab089e26f3b33ccf661a29859fdd140e644
Brian Shirey <brian.shirey@atlanticbt.com>
2015-06-26 11:43:01 -0400

    Making a more user friendly bootstrap. Warns you of impending doom.

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,32 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: brians
- * Date: 6/24/15
- * Time: 3:59 PM
+ * This is a sample bootstrap. Feel free to modify it for your purposes.
+ *
+ * Currently, this is being used to autoload the code for use in
+ * functional and unit testing. It doesn't do anything else.
+ *
+ * Well, you do get a warning if you try to
+ *
+ *
  */
-require_once('vendor/autoload.php');
-
-define('PHPUNIT_TEST_DIR', __DIR__ . '/tests');
+if (is_readable('vendor/autoload.php')) {
+    require_once('vendor/autoload.php');
+    define('PHPUNIT_TEST_DIR', __DIR__ . '/tests');
+} else {
+    echo
+        PHP_EOL .
+        '*******************' . PHP_EOL  .
+        '***** WARNING *****' . PHP_EOL .
+        '*******************' . PHP_EOL .
+        PHP_EOL .
+        'Unable to load necessary project files.' . PHP_EOL .
+        'This most likely means you did not run composer to install dependencies.' . PHP_EOL .
+        'Use `composer install` before your tests will execute.' . PHP_EOL .
+        'Unable to load necessary project files.' . PHP_EOL .
+        PHP_EOL .
+        '*******************' . PHP_EOL .
+        '***** WARNING *****' . PHP_EOL .
+        '*******************' . PHP_EOL .
+        PHP_EOL;
+    die();
+}
